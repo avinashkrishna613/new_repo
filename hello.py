cdafcb93d6d1d10557d41f86fef6b2a502124306
avinashkrishna613 <36639277+avinashkrishna613@users.noreply.github.com>
2019-12-30 12:17:48 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,11 +5,14 @@ def mul(a, b):
 	return a*b
 def sub(a, b):
 	return a-b
-
+def div(a, b):
+	return a/b
 
 one = add(1, 2)
 two = mul(2, 3)
 three = mul(4, 5)
+four = div(6, 7)
 print(one)
 print(two)
 print(three)
+print(four)
