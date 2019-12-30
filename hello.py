3cb7da2f10cddd136540ba47cbe51842445cdee1
avinashkrishna613 <avinashkrishna613@gmail.com>
2019-12-30 15:57:24 +0530

    added a new method

--- hello.py
+++ hello.py
@@ -8,11 +8,16 @@ def sub(a, b):
 def div(a, b):
 	return a/b
 
+
+def mod(a, b):
+	return a%b
 one = add(1, 2)
 two = mul(2, 3)
 three = mul(4, 5)
 four = div(6, 7)
+five = mod(1, 2)
 print(one)
 print(two)
 print(three)
 print(four)
+print(five)
