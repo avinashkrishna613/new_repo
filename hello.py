7d1e02b10e51466148575239e9fe867b57c50cdb
avinashkrishna613 <avinashkrishna613@gmail.com>
2019-12-30 11:58:16 +0530

    Third commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+def add(a, b):
+	return a+b
+
+def mul(a, b):
+	return a*b
+def sub(a, b):
+	return a-b
+
+
+one = add(1, 2)
+two = mul(2, 3)
+three = mul(4, 5)
+print(one)
+print(two)
+print(three)
